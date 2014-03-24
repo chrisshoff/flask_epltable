a843d54761c72bcc2d0b0b74c7a4c8e731697076
Chris Shoff <chris.shoff@gmail.com>
2014-03-23 21:57:37 -0400

    Move cann functionality to Flask

--- epl.py
+++ epl.py
@@ -1,5 +1,5 @@
 import requests, json
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -11,20 +11,58 @@ data_url = "http://www.chrisshoff.com:1337/teams"
 
 @app.route('/')
 def show_table():
+    """ Render the table template. Angular will 
+    handle view switching after this.
+    """
     return render_template('table.html')
 
 @app.route('/teams')
 def fetch_teams():
-    team_data = requests.get(data_url)
-    return jsonify(results=json.loads(team_data.text))
+    """ Fetch the ordered list of teams from the server
+    to display whether in standard table format, or
+    in a Cann table format.
+    """
+
+    team_data = json.loads(requests.get(data_url).text)
+    
+    # if cann variable is set, do extra processing on the team rows
+    # otherwise, return the list as is
+    if request.args.get('cann') == "true":
+        rows = [] # list of rows to return
+        last_points = team_data[0][u'points'] # keep track of last team points, starting with first team
+        i = 0
+        while i < len(team_data):
+            if last_points > team_data[i][u"points"]:
+                for j in range(0, last_points - (team_data[i][u"points"]+1)):
+                    # Append an empty row for each point between 
+                    # this team's points and the last team's points
+                    rows.append([])
+
+            this_row = [team_data[i]] # Add this team to a list representing the teams in this row
+
+            for j in range(i+1, len(team_data)):
+                if team_data[i][u"points"] == team_data[j][u"points"]:
+                    # Keep adding teams to this row, so long as their points are equal
+                    this_row.append(team_data[j])
+
+            i += len(this_row) # increment i for however many teams we added to this row
+            rows.append(this_row)
+            last_points = this_row[-1][u"points"]
+        team_data = rows
+
+    return jsonify(results=team_data)
 
 @app.route('/teams/<int:id>')
 def fetch_team(id):
+    """ Fetch a single team by its id.
+    """
     team_data = requests.get("/".join([data_url, str(id)]))
     return jsonify(results=json.loads(team_data.text))
 
 @app.route('/teams/<string:name>')
 def fetch_team_by_name(name):
+    """ Fetch a single team by its slug.
+    """
     team_data = requests.get(data_url)
     for team in json.loads(team_data.text):
        if team[u'slug'] == name:
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -16,25 +16,10 @@ eplTableControllers.controller('StandardTableCtrl', ['$scope', 'Team',
 eplTableControllers.controller('CannTableCtrl', ['$scope', 'Team',
     function($scope, Team) {
         $scope.type = 'Cann';
-        Team.query(function(data) {
-            var rows = []; // Setup array of rows
-            var last_points = data.results[0].points;
-            for (var i = 0; i < data.results.length; i++) {
-                if (last_points > data.results[i].points + 1) {
-                    for (var j = 0; j < last_points - (data.results[i].points+1); j++) {
-                        rows.push([]);
-                    }
-                }
-                var this_row = [data.results[i]];
-                for (var j = i+1; j < data.results.length; j++) {
-                    if (data.results[i].points == data.results[j].points) {
-                        this_row.push(data.results[j]);
-                    }
-                }
-
-                i += this_row.length - 1;
-                rows.push(this_row);
-                last_points = this_row[this_row.length - 1].points;
+        Team.query({ cann : true }, function(data) {
+            var rows = []
+            for (var i in data.results) {
+                rows.push(data.results[i]);
             }
 
             $scope.rows = rows;
